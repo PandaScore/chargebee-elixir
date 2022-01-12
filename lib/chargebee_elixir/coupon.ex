497dbed0918255569b212eb37c4f983294f42cac
Grant McLendon <grant@7mind.de>
2022-01-12 13:09:42 -0600

    Fix coupon paths

--- lib/chargebee_elixir/coupon.ex
+++ lib/chargebee_elixir/coupon.ex
@@ -5,22 +5,22 @@ defmodule ChargebeeElixir.Coupon do
   use ChargebeeElixir.Resource, "coupon"
 
   def create_for_items(params) do
-    create(params, "create_for_items")
+    create(params, "/create_for_items")
   end
 
   def update_for_items(coupon_id, params) do
-    post_resource(coupon_id, "update_for_items", params)
+    post_resource(coupon_id, "/update_for_items", params)
   end
 
   def delete(coupon_id) do
-    post_resource(coupon_id, "delete", %{})
+    post_resource(coupon_id, "/delete", %{})
   end
 
   def copy(coupon_id, params) do
-    post_resource(coupon_id, "copy", params)
+    post_resource(coupon_id, "/copy", params)
   end
 
   def unarchive(coupon_id) do
-    post_resource(coupon_id, "unarchive", %{})
+    post_resource(coupon_id, "/unarchive", %{})
   end
 end
