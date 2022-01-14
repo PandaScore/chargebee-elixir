695c850e37a7ccad78b8201c50f0dd745a1fffa1
Grant McLendon <grant@7mind.de>
2022-01-14 13:54:07 -0600

    Remove unused dependencies

--- mix.exs
+++ mix.exs
@@ -33,8 +33,6 @@ defmodule ChargebeeElixir.MixProject do
     [
       {:jason, "~> 1.0"},
       {:httpoison, "~> 1.7"},
-      # ! Plug should not be required for this library
-      {:plug, "~>1.11"},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
       {:mox, "~>1.0", only: [:test]},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
